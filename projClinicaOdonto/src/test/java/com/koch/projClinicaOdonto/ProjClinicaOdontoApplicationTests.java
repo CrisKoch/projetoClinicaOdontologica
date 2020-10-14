af76a9b0f811d9339d75a188e7b473f2a872bd24
CrisKoch <cristinak.iftm@gmail.com>
2020-10-14 20:31:03 -0300

    Criação do projeto

---
+++ projClinicaOdonto/src/test/java/com/koch/projClinicaOdonto/ProjClinicaOdontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koch.projClinicaOdonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjClinicaOdontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
