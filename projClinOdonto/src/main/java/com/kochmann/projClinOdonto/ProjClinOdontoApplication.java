915f0cba75a1671a7162056618c81a190495e84e
CrisKoch <cristinak.iftm@gmail.com>
2020-10-14 20:11:21 -0300

    Criação do Projeto MVC

---
+++ projClinOdonto/src/main/java/com/kochmann/projClinOdonto/ProjClinOdontoApplication.java
@@ -0,0 +1,13 @@
+package com.kochmann.projClinOdonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjClinOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjClinOdontoApplication.class, args);
+	}
+
+}
