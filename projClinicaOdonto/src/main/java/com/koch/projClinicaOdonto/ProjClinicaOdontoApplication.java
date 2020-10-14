52691c5eaed30406493fec4b5cc13a9ca54fff3e
CrisKoch <cristinak.iftm@gmail.com>
2020-10-14 20:23:49 -0300

    Primeiro commit - criação do projeto

---
+++ projClinicaOdonto/src/main/java/com/koch/projClinicaOdonto/ProjClinicaOdontoApplication.java
@@ -0,0 +1,13 @@
+package com.koch.projClinicaOdonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjClinicaOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjClinicaOdontoApplication.class, args);
+	}
+
+}
